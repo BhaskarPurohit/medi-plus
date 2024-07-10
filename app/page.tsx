b9361b3137e000dbd16689f833ddcf44bcd0d36f
BhaskarPurohit <bhaskarpurohit22@gmail.com>
2024-07-10 18:07:50 +0530

    public folder added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
-
+import mediPlusLogo from "./medi-plus-logo.png"
 export default function Home() {
   return (
-    <div className="text-3xl underline">
-      <h1>Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src={mediPlusLogo}
+            alt="Medi-Plus"
+          />
+        </div>
+      </section>
     </div>
   );
 }
